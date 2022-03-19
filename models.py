9d69fffdc23d2514154396ad42f14b490cc90bf2
Fatemeh Dehghan <fdghn77@gmail.com>
2022-03-19 09:45:24 +0330

    add file models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from unicodedata import name
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class Students(Base):
+    __tablename__ = "students"
+
+    name = Column(String, primary_key=True, index=True)
+    tarikh = Column(Integer)
+    zaban = Column(Integer)
+ 
